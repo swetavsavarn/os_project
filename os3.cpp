9b36b88b87bd693c86214f3076c1ba81c48d7a22
swetavsavarn <33478489+swetavsavarn@users.noreply.github.com>
2019-04-14 12:30:24 +0530

    Add files via upload

---
+++ os3.cpp
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int et[30],ts,n,i,x=0,tot=0;
+char pn[10][10];
+
+printf("Enter the no of processes:");
+scanf("%d",&n);
+printf("Enter the time quantum:");
+scanf("%d",&ts);
+for(i=0;i<n;{
+printf("enter process name & estimated time:");
+scanf("%s %d",pn[i],&et[i]);
+}
+printf("The processes are:");
+for(i=0;i<n;i++)
+printf("process %d: %s\n",i+1,pn[i]);
+for(i=0;i<n;i++)
+tot=tot+et[
+#include<conio.h>
+int main()
+{
+int et[30],ts,n,i,x=0,tot=0;
+char pn[10][10];
+
+printf("Enter the no of processes:");
+scanf("%d",&n);
+printf("Enter the time quantum:");
+scanf("%d",&ts);
+for(i=0;i<n;i++)
+{
+printf("enter process name & estimated time:");
+scanf("%s %d",pn[i],&et[i]);
+}
+printf("The processes are:");
+for(i=0;i<n;i++)
+printf("process %d: %s\n",i+1,pn[i]);
+for(i=0;i<n;i++)
+tot=tot+et[i];
+while(x!=tot)
+{
+for(i=0;i<n;i++)
+{
+if(et[i]>ts)
+{
+x=x+ts;
+printf("\n %s -> %d",pn[i],ts);
+et[i]=et[i]-ts;
+}
+else
